jayarnim/M1-PJT_DL

experiemnt completed

src/fm/cin.py
@@ -1,0 +1,52 @@
+import torch
+import torch.nn as nn
+from .components.linear import LinearLayer
+from .components.cin import CompressedInteractionNetworksLayer
+from ctr.model.base import BaseModel
+from ctr.model.feature_embedding import build_feature_embedding
+from ctr.featuremap import FeatureMap
+
+
+class CompressedInteractionNetworks(BaseModel):
+    def __init__(
+        self,
+        embedding_dim: int, 
+        channels: list[int],
+        dropout: float,
+        feature_map: FeatureMap,
+    ):
+        super().__init__(locals())
+
+        self.weight = build_feature_embedding(
+            embedding_dim=1,
+            feature_map=feature_map,
+        )
+        self.linear = LinearLayer()
+
+        self.embedding = build_feature_embedding(
+            embedding_dim=embedding_dim,
+            feature_map=feature_map,
+        )
+        self.cin = CompressedInteractionNetworksLayer(
+            dim=embedding_dim,
+            in_channels=len(feature_map),
+            out_channels=channels,
+            dropout=dropout,
+        )
+
+        self.bias = nn.Parameter(
+            data=torch.zeros(1),
+            requires_grad=True,
+        )
+
+    def forward(
+        self, 
+        X: torch.Tensor,
+    ) -> torch.Tensor:
+        weights = self.weight(X)
+        main_effect = self.linear(weights)
+
+        embeddings = self.embedding(X)
+        interaction_effect = self.cin(embeddings)
+
+        return self.bias + main_effect + interaction_effect

src/fm/deepfm.py
@@ -1,0 +1,56 @@
+import torch
+import torch.nn as nn
+from .components.linear import LinearLayer
+from .components.fm import FactorizationMachineLayer
+from .components.dnn import DeepNeuralNetworksLayer
+from ctr.model.base import BaseModel
+from ctr.model.feature_embedding import build_feature_embedding
+from ctr.featuremap import FeatureMap
+
+
+class DeepFactorizationMachine(BaseModel):
+    def __init__(
+        self,
+        embedding_dim: int, 
+        hidden_dim: list[int],
+        dropout: float,
+        feature_map: FeatureMap,
+    ):
+        super().__init__(locals())
+
+        self.weight = build_feature_embedding(
+            embedding_dim=1,
+            feature_map=feature_map,
+        )
+        self.linear = LinearLayer()
+
+        self.embedding = build_feature_embedding(
+            embedding_dim=embedding_dim,
+            feature_map=feature_map,
+        )
+        self.fm = FactorizationMachineLayer()
+        self.dnn = DeepNeuralNetworksLayer(
+            input_dim=embedding_dim*len(feature_map),
+            hidden_dim=hidden_dim,
+            dropout=dropout,
+        )
+
+        self.bias = nn.Parameter(
+            data=torch.zeros(1),
+            requires_grad=True,
+        )
+
+    def forward(
+        self, 
+        X: torch.Tensor,
+    ) -> torch.Tensor:
+        weights = self.weight(X)
+        main_effect = self.linear(weights)
+
+        embeddings = self.embedding(X)
+        interaction_effect = (
+            self.fm(embeddings) 
+            + self.dnn(embeddings)
+        )
+
+        return self.bias + main_effect + interaction_effect

src/fm/dnn.py
@@ -1,0 +1,51 @@
+import torch
+import torch.nn as nn
+from .components.linear import LinearLayer
+from .components.dnn import DeepNeuralNetworksLayer
+from ctr.model.base import BaseModel
+from ctr.model.feature_embedding import build_feature_embedding
+from ctr.featuremap import FeatureMap
+
+
+class DeepNeuralNetworks(BaseModel):
+    def __init__(
+        self,
+        embedding_dim: int, 
+        hidden_dim: list[int],
+        dropout: float,
+        feature_map: FeatureMap,
+    ):
+        super().__init__(locals())
+
+        self.weight = build_feature_embedding(
+            embedding_dim=1,
+            feature_map=feature_map,
+        )
+        self.linear = LinearLayer()
+
+        self.embedding = build_feature_embedding(
+            embedding_dim=embedding_dim,
+            feature_map=feature_map,
+        )
+        self.dnn = DeepNeuralNetworksLayer(
+            input_dim=embedding_dim*len(feature_map),
+            hidden_dim=hidden_dim,
+            dropout=dropout,
+        )
+
+        self.bias = nn.Parameter(
+            data=torch.zeros(1),
+            requires_grad=True,
+        )
+
+    def forward(
+        self, 
+        X: torch.Tensor,
+    ) -> torch.Tensor:
+        weights = self.weight(X)
+        main_effect = self.linear(weights)
+
+        embeddings = self.embedding(X)
+        interaction_effect = self.dnn(embeddings)
+
+        return self.bias + main_effect + interaction_effect

src/fm/extremednn.py
@@ -1,0 +1,62 @@
+import torch
+import torch.nn as nn
+from .components.linear import LinearLayer
+from .components.dnn import DeepNeuralNetworksLayer
+from .components.cin import CompressedInteractionNetworksLayer
+from ctr.model.base import BaseModel
+from ctr.model.feature_embedding import build_feature_embedding
+from ctr.featuremap import FeatureMap
+
+
+class eXtremeDeepNeuralNetworks(BaseModel):
+    def __init__(
+        self,
+        embedding_dim: int, 
+        hidden_dim: list[int],
+        channels: list[int],
+        dropout: float,
+        feature_map: FeatureMap,
+    ):
+        super().__init__(locals())
+
+        self.weight = build_feature_embedding(
+            embedding_dim=1,
+            feature_map=feature_map,
+        )
+        self.linear = LinearLayer()
+
+        self.embedding = build_feature_embedding(
+            embedding_dim=embedding_dim,
+            feature_map=feature_map,
+        )
+        self.dnn = DeepNeuralNetworksLayer(
+            input_dim=embedding_dim*len(feature_map),
+            hidden_dim=hidden_dim,
+            dropout=dropout,
+        )
+        self.cin = CompressedInteractionNetworksLayer(
+            dim=embedding_dim,
+            in_channels=len(feature_map),
+            out_channels=channels,
+            dropout=dropout,
+        )
+
+        self.bias = nn.Parameter(
+            data=torch.zeros(1),
+            requires_grad=True,
+        )
+
+    def forward(
+        self, 
+        X: torch.Tensor,
+    ) -> torch.Tensor:
+        weights = self.weight(X)
+        main_effect = self.linear(weights)
+
+        embeddings = self.embedding(X)
+        interaction_effect = (
+            self.dnn(embeddings) 
+            + self.cin(embeddings)
+        )
+
+        return self.bias + main_effect + interaction_effect

src/fm/extremefm.py
@@ -1,0 +1,57 @@
+import torch
+import torch.nn as nn
+from .components.linear import LinearLayer
+from .components.fm import FactorizationMachineLayer
+from .components.cin import CompressedInteractionNetworksLayer
+from ctr.model.base import BaseModel
+from ctr.model.feature_embedding import build_feature_embedding
+from ctr.featuremap import FeatureMap
+
+
+class eXtremeFactorizationMachine(BaseModel):
+    def __init__(
+        self,
+        embedding_dim: int, 
+        channels: list[int],
+        dropout: float,
+        feature_map: FeatureMap,
+    ):
+        super().__init__(locals())
+
+        self.weight = build_feature_embedding(
+            embedding_dim=1,
+            feature_map=feature_map,
+        )
+        self.linear = LinearLayer()
+
+        self.embedding = build_feature_embedding(
+            embedding_dim=embedding_dim,
+            feature_map=feature_map,
+        )
+        self.fm = FactorizationMachineLayer()
+        self.cin = CompressedInteractionNetworksLayer(
+            dim=embedding_dim,
+            in_channels=len(feature_map),
+            out_channels=channels,
+            dropout=dropout,
+        )
+
+        self.bias = nn.Parameter(
+            data=torch.zeros(1),
+            requires_grad=True,
+        )
+
+    def forward(
+        self, 
+        X: torch.Tensor,
+    ) -> torch.Tensor:
+        weights = self.weight(X)
+        main_effect = self.linear(weights)
+
+        embeddings = self.embedding(X)
+        interaction_effect = (
+            self.fm(embeddings) 
+            + self.cin(embeddings)
+        )
+
+        return self.bias + main_effect + interaction_effect

src/fm/fm.py
@@ -1,0 +1,45 @@
+import torch
+import torch.nn as nn
+from .components.linear import LinearLayer
+from .components.fm import FactorizationMachineLayer
+from ctr.model.base import BaseModel
+from ctr.model.feature_embedding import build_feature_embedding
+from ctr.featuremap import FeatureMap
+
+
+class FactorizationMachine(BaseModel):
+    def __init__(
+        self,
+        embedding_dim: int, 
+        feature_map: FeatureMap,
+    ):
+        super().__init__(locals())
+
+        self.weight = build_feature_embedding(
+            embedding_dim=1,
+            feature_map=feature_map,
+        )
+        self.linear = LinearLayer()
+
+        self.embedding = build_feature_embedding(
+            embedding_dim=embedding_dim,
+            feature_map=feature_map,
+        )
+        self.fm = FactorizationMachineLayer()
+
+        self.bias = nn.Parameter(
+            data=torch.zeros(1),
+            requires_grad=True,
+        )
+
+    def forward(
+        self, 
+        X: torch.Tensor,
+    ) -> torch.Tensor:
+        weights = self.weight(X)
+        main_effect = self.linear(weights)
+
+        embeddings = self.embedding(X)
+        interaction_effect = self.fm(embeddings)
+
+        return self.bias + main_effect + interaction_effect

src/fm/xdeepfm.py
@@ -1,0 +1,65 @@
+import torch
+import torch.nn as nn
+from .components.linear import LinearLayer
+from .components.fm import FactorizationMachineLayer
+from .components.dnn import DeepNeuralNetworksLayer
+from .components.cin import CompressedInteractionNetworksLayer
+from ctr.model.base import BaseModel
+from ctr.model.feature_embedding import build_feature_embedding
+from ctr.featuremap import FeatureMap
+
+
+class eXtremeDeepFactorizationMachine(BaseModel):
+    def __init__(
+        self,
+        embedding_dim: int, 
+        hidden_dim: list[int],
+        channels: list[int],
+        dropout: float,
+        feature_map: FeatureMap,
+    ):
+        super().__init__(locals())
+
+        self.weight = build_feature_embedding(
+            embedding_dim=1,
+            feature_map=feature_map,
+        )
+        self.linear = LinearLayer()
+
+        self.embedding = build_feature_embedding(
+            embedding_dim=embedding_dim,
+            feature_map=feature_map,
+        )
+        self.fm = FactorizationMachineLayer()
+        self.dnn = DeepNeuralNetworksLayer(
+            input_dim=embedding_dim*len(feature_map),
+            hidden_dim=hidden_dim,
+            dropout=dropout,
+        )
+        self.cin = CompressedInteractionNetworksLayer(
+            dim=embedding_dim,
+            in_channels=len(feature_map),
+            out_channels=channels,
+            dropout=dropout,
+        )
+
+        self.bias = nn.Parameter(
+            data=torch.zeros(1),
+            requires_grad=True,
+        )
+
+    def forward(
+        self, 
+        X: torch.Tensor,
+    ) -> torch.Tensor:
+        weights = self.weight(X)
+        main_effect = self.linear(weights)
+
+        embeddings = self.embedding(X)
+        interaction_effect = (
+            self.fm(embeddings) 
+            + self.dnn(embeddings) 
+            + self.cin(embeddings)
+        )
+
+        return self.bias + main_effect + interaction_effect

jayarnim/M1-PJT-DL

src/fm/cin.py
@@ -1,0 +1,52 @@
+import torch
+import torch.nn as nn
+from .layers.linear import LinearLayer
+from .layers.cin import CompressedInteractionNetworksLayer
+from components.base import BaseModel
+from components.feature_embedding import build as build_feature_embedding
+from components.feature_map import FeatureMap
+
+
+class CompressedInteractionNetworks(BaseModel):
+    def __init__(
+        self,
+        embedding_dim: int, 
+        channels: list[int],
+        dropout: float,
+        feature_map: FeatureMap,
+    ):
+        super().__init__(locals())
+
+        self.weight = build_feature_embedding(
+            embedding_dim=1,
+            feature_map=feature_map,
+        )
+        self.linear = LinearLayer()
+
+        self.embedding = build_feature_embedding(
+            embedding_dim=embedding_dim,
+            feature_map=feature_map,
+        )
+        self.cin = CompressedInteractionNetworksLayer(
+            dim=embedding_dim,
+            in_channels=len(feature_map),
+            out_channels=channels,
+            dropout=dropout,
+        )
+
+        self.bias = nn.Parameter(
+            data=torch.zeros(1),
+            requires_grad=True,
+        )
+
+    def forward(
+        self, 
+        X: torch.Tensor,
+    ) -> torch.Tensor:
+        weights = self.weight(X)
+        main_effect = self.linear(weights)
+
+        embeddings = self.embedding(X)
+        interaction_effect = self.cin(embeddings)
+
+        return self.bias + main_effect + interaction_effect

src/fm/deepfm.py
@@ -1,0 +1,56 @@
+import torch
+import torch.nn as nn
+from .layers.linear import LinearLayer
+from .layers.fm import FactorizationMachineLayer
+from .layers.dnn import DeepNeuralNetworksLayer
+from components.base import BaseModel
+from components.feature_embedding import build as build_feature_embedding
+from components.feature_map import FeatureMap
+
+
+class DeepFactorizationMachine(BaseModel):
+    def __init__(
+        self,
+        embedding_dim: int, 
+        hidden_dim: list[int],
+        dropout: float,
+        feature_map: FeatureMap,
+    ):
+        super().__init__(locals())
+
+        self.weight = build_feature_embedding(
+            embedding_dim=1,
+            feature_map=feature_map,
+        )
+        self.linear = LinearLayer()
+
+        self.embedding = build_feature_embedding(
+            embedding_dim=embedding_dim,
+            feature_map=feature_map,
+        )
+        self.fm = FactorizationMachineLayer()
+        self.dnn = DeepNeuralNetworksLayer(
+            input_dim=embedding_dim*len(feature_map),
+            hidden_dim=hidden_dim,
+            dropout=dropout,
+        )
+
+        self.bias = nn.Parameter(
+            data=torch.zeros(1),
+            requires_grad=True,
+        )
+
+    def forward(
+        self, 
+        X: torch.Tensor,
+    ) -> torch.Tensor:
+        weights = self.weight(X)
+        main_effect = self.linear(weights)
+
+        embeddings = self.embedding(X)
+        interaction_effect = (
+            self.fm(embeddings) 
+            + self.dnn(embeddings)
+        )
+
+        return self.bias + main_effect + interaction_effect

src/fm/dnn.py
@@ -1,0 +1,51 @@
+import torch
+import torch.nn as nn
+from .layers.linear import LinearLayer
+from .layers.dnn import DeepNeuralNetworksLayer
+from components.base import BaseModel
+from components.feature_embedding import build as build_feature_embedding
+from components.feature_map import FeatureMap
+
+
+class DeepNeuralNetworks(BaseModel):
+    def __init__(
+        self,
+        embedding_dim: int, 
+        hidden_dim: list[int],
+        dropout: float,
+        feature_map: FeatureMap,
+    ):
+        super().__init__(locals())
+
+        self.weight = build_feature_embedding(
+            embedding_dim=1,
+            feature_map=feature_map,
+        )
+        self.linear = LinearLayer()
+
+        self.embedding = build_feature_embedding(
+            embedding_dim=embedding_dim,
+            feature_map=feature_map,
+        )
+        self.dnn = DeepNeuralNetworksLayer(
+            input_dim=embedding_dim*len(feature_map),
+            hidden_dim=hidden_dim,
+            dropout=dropout,
+        )
+
+        self.bias = nn.Parameter(
+            data=torch.zeros(1),
+            requires_grad=True,
+        )
+
+    def forward(
+        self, 
+        X: torch.Tensor,
+    ) -> torch.Tensor:
+        weights = self.weight(X)
+        main_effect = self.linear(weights)
+
+        embeddings = self.embedding(X)
+        interaction_effect = self.dnn(embeddings)
+
+        return self.bias + main_effect + interaction_effect

src/fm/extremednn.py
@@ -1,0 +1,62 @@
+import torch
+import torch.nn as nn
+from .layers.linear import LinearLayer
+from .layers.dnn import DeepNeuralNetworksLayer
+from .layers.cin import CompressedInteractionNetworksLayer
+from components.base import BaseModel
+from components.feature_embedding import build as build_feature_embedding
+from components.feature_map import FeatureMap
+
+
+class eXtremeDeepNeuralNetworks(BaseModel):
+    def __init__(
+        self,
+        embedding_dim: int, 
+        hidden_dim: list[int],
+        channels: list[int],
+        dropout: float,
+        feature_map: FeatureMap,
+    ):
+        super().__init__(locals())
+
+        self.weight = build_feature_embedding(
+            embedding_dim=1,
+            feature_map=feature_map,
+        )
+        self.linear = LinearLayer()
+
+        self.embedding = build_feature_embedding(
+            embedding_dim=embedding_dim,
+            feature_map=feature_map,
+        )
+        self.dnn = DeepNeuralNetworksLayer(
+            input_dim=embedding_dim*len(feature_map),
+            hidden_dim=hidden_dim,
+            dropout=dropout,
+        )
+        self.cin = CompressedInteractionNetworksLayer(
+            dim=embedding_dim,
+            in_channels=len(feature_map),
+            out_channels=channels,
+            dropout=dropout,
+        )
+
+        self.bias = nn.Parameter(
+            data=torch.zeros(1),
+            requires_grad=True,
+        )
+
+    def forward(
+        self, 
+        X: torch.Tensor,
+    ) -> torch.Tensor:
+        weights = self.weight(X)
+        main_effect = self.linear(weights)
+
+        embeddings = self.embedding(X)
+        interaction_effect = (
+            self.dnn(embeddings) 
+            + self.cin(embeddings)
+        )
+
+        return self.bias + main_effect + interaction_effect

src/fm/extremefm.py
@@ -1,0 +1,57 @@
+import torch
+import torch.nn as nn
+from .layers.linear import LinearLayer
+from .layers.fm import FactorizationMachineLayer
+from .layers.cin import CompressedInteractionNetworksLayer
+from components.base import BaseModel
+from components.feature_embedding import build as build_feature_embedding
+from components.feature_map import FeatureMap
+
+
+class eXtremeFactorizationMachine(BaseModel):
+    def __init__(
+        self,
+        embedding_dim: int, 
+        channels: list[int],
+        dropout: float,
+        feature_map: FeatureMap,
+    ):
+        super().__init__(locals())
+
+        self.weight = build_feature_embedding(
+            embedding_dim=1,
+            feature_map=feature_map,
+        )
+        self.linear = LinearLayer()
+
+        self.embedding = build_feature_embedding(
+            embedding_dim=embedding_dim,
+            feature_map=feature_map,
+        )
+        self.fm = FactorizationMachineLayer()
+        self.cin = CompressedInteractionNetworksLayer(
+            dim=embedding_dim,
+            in_channels=len(feature_map),
+            out_channels=channels,
+            dropout=dropout,
+        )
+
+        self.bias = nn.Parameter(
+            data=torch.zeros(1),
+            requires_grad=True,
+        )
+
+    def forward(
+        self, 
+        X: torch.Tensor,
+    ) -> torch.Tensor:
+        weights = self.weight(X)
+        main_effect = self.linear(weights)
+
+        embeddings = self.embedding(X)
+        interaction_effect = (
+            self.fm(embeddings) 
+            + self.cin(embeddings)
+        )
+
+        return self.bias + main_effect + interaction_effect

src/fm/fm.py
@@ -1,0 +1,45 @@
+import torch
+import torch.nn as nn
+from .layers.linear import LinearLayer
+from .layers.fm import FactorizationMachineLayer
+from components.base import BaseModel
+from components.feature_embedding import build as build_feature_embedding
+from components.feature_map import FeatureMap
+
+
+class FactorizationMachine(BaseModel):
+    def __init__(
+        self,
+        embedding_dim: int, 
+        feature_map: FeatureMap,
+    ):
+        super().__init__(locals())
+
+        self.weight = build_feature_embedding(
+            embedding_dim=1,
+            feature_map=feature_map,
+        )
+        self.linear = LinearLayer()
+
+        self.embedding = build_feature_embedding(
+            embedding_dim=embedding_dim,
+            feature_map=feature_map,
+        )
+        self.fm = FactorizationMachineLayer()
+
+        self.bias = nn.Parameter(
+            data=torch.zeros(1),
+            requires_grad=True,
+        )
+
+    def forward(
+        self, 
+        X: torch.Tensor,
+    ) -> torch.Tensor:
+        weights = self.weight(X)
+        main_effect = self.linear(weights)
+
+        embeddings = self.embedding(X)
+        interaction_effect = self.fm(embeddings)
+
+        return self.bias + main_effect + interaction_effect

src/fm/xdeepfm.py
@@ -1,0 +1,65 @@
+import torch
+import torch.nn as nn
+from .layers.linear import LinearLayer
+from .layers.fm import FactorizationMachineLayer
+from .layers.dnn import DeepNeuralNetworksLayer
+from .layers.cin import CompressedInteractionNetworksLayer
+from components.base import BaseModel
+from components.feature_embedding import build as build_feature_embedding
+from components.feature_map import FeatureMap
+
+
+class eXtremeDeepFactorizationMachine(BaseModel):
+    def __init__(
+        self,
+        embedding_dim: int, 
+        hidden_dim: list[int],
+        channels: list[int],
+        dropout: float,
+        feature_map: FeatureMap,
+    ):
+        super().__init__(locals())
+
+        self.weight = build_feature_embedding(
+            embedding_dim=1,
+            feature_map=feature_map,
+        )
+        self.linear = LinearLayer()
+
+        self.embedding = build_feature_embedding(
+            embedding_dim=embedding_dim,
+            feature_map=feature_map,
+        )
+        self.fm = FactorizationMachineLayer()
+        self.dnn = DeepNeuralNetworksLayer(
+            input_dim=embedding_dim*len(feature_map),
+            hidden_dim=hidden_dim,
+            dropout=dropout,
+        )
+        self.cin = CompressedInteractionNetworksLayer(
+            dim=embedding_dim,
+            in_channels=len(feature_map),
+            out_channels=channels,
+            dropout=dropout,
+        )
+
+        self.bias = nn.Parameter(
+            data=torch.zeros(1),
+            requires_grad=True,
+        )
+
+    def forward(
+        self, 
+        X: torch.Tensor,
+    ) -> torch.Tensor:
+        weights = self.weight(X)
+        main_effect = self.linear(weights)
+
+        embeddings = self.embedding(X)
+        interaction_effect = (
+            self.fm(embeddings) 
+            + self.dnn(embeddings) 
+            + self.cin(embeddings)
+        )
+
+        return self.bias + main_effect + interaction_effect